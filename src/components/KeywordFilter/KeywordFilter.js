rvasqz86/flex-template-web

Create KeywordFilter Add it to desktop and mobile layout

src/components/KeywordFilter/KeywordFilter.js
@@ -1,0 +1,214 @@
+import React, { Component } from 'react';
+import { func, number, string } from 'prop-types';
+import classNames from 'classnames';
+import { injectIntl, intlShape } from 'react-intl';
+import debounce from 'lodash/debounce';
+import { FieldTextInput } from '../../components';
+
+import { FilterPopup, FilterPlain } from '../../components';
+import css from './KeywordFilter.css';
+
+// When user types, we wait for new keystrokes a while before searching new content
+const DEBOUNCE_WAIT_TIME = 600;
+// Short search queries (e.g. 2 letters) have a longer timeout before search is made
+const TIMEOUT_FOR_SHORT_QUERIES = 2000;
+
+class KeywordFilter extends Component {
+  constructor(props) {
+    super(props);
+
+    this.filter = null;
+    this.filterContent = null;
+    this.shortKeywordTimeout = null;
+    this.inputRef = React.createRef();
+
+    this.positionStyleForContent = this.positionStyleForContent.bind(this);
+  }
+
+  componentWillUnmount() {
+    window.clearTimeout(this.shortKeywordTimeout);
+  }
+
+  positionStyleForContent() {
+    if (this.filter && this.filterContent) {
+      // Render the filter content to the right from the menu
+      // unless there's no space in which case it is rendered
+      // to the left
+      const distanceToRight = window.innerWidth - this.filter.getBoundingClientRect().right;
+      const labelWidth = this.filter.offsetWidth;
+      const contentWidth = this.filterContent.offsetWidth;
+      const contentWidthBiggerThanLabel = contentWidth - labelWidth;
+      const renderToRight = distanceToRight > contentWidthBiggerThanLabel;
+      const contentPlacementOffset = this.props.contentPlacementOffset;
+
+      const offset = renderToRight
+        ? { left: contentPlacementOffset }
+        : { right: contentPlacementOffset };
+      // set a min-width if the content is narrower than the label
+      const minWidth = contentWidth < labelWidth ? { minWidth: labelWidth } : null;
+
+      return { ...offset, ...minWidth };
+    }
+    return {};
+  }
+
+  render() {
+    const {
+      rootClassName,
+      className,
+      id,
+      name,
+      label,
+      initialValues,
+      contentPlacementOffset,
+      onSubmit,
+      urlParam,
+      intl,
+      showAsPopup,
+      ...rest
+    } = this.props;
+
+    const classes = classNames(rootClassName || css.root, className);
+
+    const hasInitialValues = !!initialValues && initialValues.length > 0;
+    const labelForPopup = hasInitialValues
+      ? intl.formatMessage({ id: 'KeywordFilter.labelSelected' }, { labelText: initialValues })
+      : label;
+
+    const labelForPlain = hasInitialValues
+      ? intl.formatMessage(
+          { id: 'KeywordFilterPlainForm.labelSelected' },
+          { labelText: initialValues }
+        )
+      : label;
+
+    const filterText = intl.formatMessage({ id: 'KeywordFilter.filterText' });
+
+    const placeholder = intl.formatMessage({ id: 'KeywordFilter.placeholder' });
+
+    const contentStyle = this.positionStyleForContent();
+
+    // pass the initial values with the name key so that
+    // they can be passed to the correct field
+    const namedInitialValues = { [name]: initialValues };
+
+    const handleSubmit = (urlParam, values) => {
+      const usedValue = values ? values[name] : values;
+      onSubmit(urlParam, usedValue);
+    };
+
+    const debouncedSubmit = debounce(handleSubmit, DEBOUNCE_WAIT_TIME, {
+      leading: false,
+      trailing: true,
+    });
+    // Use timeout for shart queries and debounce for queries with any length
+    const handleChangeWithDebounce = (urlParam, values) => {
+      // handleSubmit gets urlParam and values as params.
+      // If this field ("keyword") is short, create timeout
+      const hasKeywordValue = values && values[name];
+      const keywordValue = hasKeywordValue ? values && values[name] : '';
+      if (urlParam && (!hasKeywordValue || (hasKeywordValue && keywordValue.length >= 3))) {
+        if (this.shortKeywordTimeout) {
+          window.clearTimeout(this.shortKeywordTimeout);
+        }
+        return debouncedSubmit(urlParam, values);
+      } else {
+        this.shortKeywordTimeout = window.setTimeout(() => {
+          // if inputRef exists, use the most up-to-date value from there
+          return this.inputRef && this.inputRef.current
+            ? handleSubmit(urlParam, { ...values, [name]: this.inputRef.current.value })
+            : handleSubmit(urlParam, values);
+        }, TIMEOUT_FOR_SHORT_QUERIES);
+      }
+    };
+
+    // Uncontrolled input needs to be cleared through the reference to DOM element.
+    const handleClear = () => {
+      if (this.inputRef && this.inputRef.current) {
+        this.inputRef.current.value = '';
+      }
+    };
+
+    return showAsPopup ? (
+      <FilterPopup
+        className={classes}
+        rootClassName={rootClassName}
+        popupClassName={css.popupSize}
+        name={name}
+        label={labelForPopup}
+        isSelected={hasInitialValues}
+        id={`${id}.popup`}
+        showAsPopup
+        labelMaxWidth={250}
+        contentPlacementOffset={contentPlacementOffset}
+        onSubmit={handleSubmit}
+        initialValues={namedInitialValues}
+        urlParam={urlParam}
+        keepDirtyOnReinitialize
+        {...rest}
+      >
+        <FieldTextInput
+          className={css.field}
+          name={name}
+          id={`${id}-input`}
+          type="text"
+          label={filterText}
+          placeholder={placeholder}
+          autoComplete="off"
+        />
+      </FilterPopup>
+    ) : (
+      <FilterPlain
+        className={className}
+        rootClassName={rootClassName}
+        label={labelForPlain}
+        isSelected={hasInitialValues}
+        id={`${id}.plain`}
+        liveEdit
+        contentPlacementOffset={contentStyle}
+        onSubmit={handleChangeWithDebounce}
+        onClear={handleClear}
+        initialValues={namedInitialValues}
+        urlParam={urlParam}
+        {...rest}
+      >
+        <fieldset className={css.fieldPlain}>
+          <label>{filterText}</label>
+          <FieldTextInput
+            name={name}
+            id={`${id}-input`}
+            isUncontrolled
+            inputRef={this.inputRef}
+            type="text"
+            placeholder={placeholder}
+            autoComplete="off"
+          />
+        </fieldset>
+      </FilterPlain>
+    );
+  }
+}
+
+KeywordFilter.defaultProps = {
+  rootClassName: null,
+  className: null,
+  initialValues: null,
+  contentPlacementOffset: 0,
+};
+
+KeywordFilter.propTypes = {
+  rootClassName: string,
+  className: string,
+  id: string.isRequired,
+  name: string.isRequired,
+  urlParam: string.isRequired,
+  label: string.isRequired,
+  onSubmit: func.isRequired,
+  initialValues: string,
+  contentPlacementOffset: number,
+
+  // form injectIntl
+  intl: intlShape.isRequired,
+};
+
+export default injectIntl(KeywordFilter);

src/components/SearchFiltersMobile/SearchFiltersMobile.js
@@ -11,6 +11,7 @@ import { createResourceLocatorString } from '../../util/routes';
 import {
   ModalInMobile,
   Button,
+  KeywordFilter,
   PriceFilter,
   SelectSingleFilter,
   SelectMultipleFilter,
@@ -34,6 +35,7 @@ class SearchFiltersMobileComponent extends Component {
     this.handleSelectMultiple = this.handleSelectMultiple.bind(this);
     this.handlePrice = this.handlePrice.bind(this);
     this.handleDateRange = this.handleDateRange.bind(this);
+    this.handleKeyword = this.handleKeyword.bind(this);
     this.initialValue = this.initialValue.bind(this);
     this.initialValues = this.initialValues.bind(this);
     this.initialPriceRangeValue = this.initialPriceRangeValue.bind(this);
@@ -118,6 +120,15 @@ class SearchFiltersMobileComponent extends Component {
     history.push(createResourceLocatorString('SearchPage', routeConfiguration(), {}, queryParams));
   }
 
+  handleKeyword(urlParam, keywords) {
+    const { urlQueryParams, history } = this.props;
+    const queryParams = urlParam
+      ? { ...urlQueryParams, [urlParam]: keywords }
+      : omit(urlQueryParams, urlParam);
+
+    history.push(createResourceLocatorString('SearchPage', routeConfiguration(), {}, queryParams));
+  }
+
   // Reset all filter query parameters
   resetAll(e) {
     const { urlQueryParams, history, filterParamNames } = this.props;
@@ -185,6 +196,7 @@ class SearchFiltersMobileComponent extends Component {
       amenitiesFilter,
       priceFilter,
       dateRangeFilter,
+      keywordFilter,
       intl,
     } = this.props;
 
@@ -267,6 +279,24 @@ class SearchFiltersMobileComponent extends Component {
         />
       ) : null;
 
+    const initialKeyword = this.initialValue(keywordFilter.paramName);
+    const keywordLabel = intl.formatMessage({
+      id: 'SearchFilters.keywordLabel',
+    });
+    const keywordFilterElement =
+      keywordFilter && keywordFilter.config.active ? (
+        <KeywordFilter
+          id={'SearchFilters.keywordFilter'}
+          name="keyword"
+          urlParam={keywordFilter.paramName}
+          label={keywordLabel}
+          onSubmit={this.handleKeyword}
+          liveEdit
+          showAsPopup={false}
+          initialValues={initialKeyword}
+        />
+      ) : null;
+
     return (
       <div className={classes}>
         <div className={css.searchResultSummary}>
@@ -299,6 +329,7 @@ class SearchFiltersMobileComponent extends Component {
           </div>
           {this.state.isFiltersOpenOnMobile ? (
             <div className={css.filtersWrapper}>
+              {keywordFilterElement}
               {categoryFilterElement}
               {amenitiesFilterElement}
               {priceFilterElement}

embryCODE/nashville-for-hire

src/components/KeywordFilter/KeywordFilter.js
@@ -1,0 +1,214 @@
+import React, { Component } from 'react';
+import { func, number, string } from 'prop-types';
+import classNames from 'classnames';
+import { injectIntl, intlShape } from 'react-intl';
+import debounce from 'lodash/debounce';
+import { FieldTextInput } from '../../components';
+
+import { FilterPopup, FilterPlain } from '../../components';
+import css from './KeywordFilter.css';
+
+// When user types, we wait for new keystrokes a while before searching new content
+const DEBOUNCE_WAIT_TIME = 600;
+// Short search queries (e.g. 2 letters) have a longer timeout before search is made
+const TIMEOUT_FOR_SHORT_QUERIES = 2000;
+
+class KeywordFilter extends Component {
+  constructor(props) {
+    super(props);
+
+    this.filter = null;
+    this.filterContent = null;
+    this.shortKeywordTimeout = null;
+    this.mobileInputRef = React.createRef();
+
+    this.positionStyleForContent = this.positionStyleForContent.bind(this);
+  }
+
+  componentWillUnmount() {
+    window.clearTimeout(this.shortKeywordTimeout);
+  }
+
+  positionStyleForContent() {
+    if (this.filter && this.filterContent) {
+      // Render the filter content to the right from the menu
+      // unless there's no space in which case it is rendered
+      // to the left
+      const distanceToRight = window.innerWidth - this.filter.getBoundingClientRect().right;
+      const labelWidth = this.filter.offsetWidth;
+      const contentWidth = this.filterContent.offsetWidth;
+      const contentWidthBiggerThanLabel = contentWidth - labelWidth;
+      const renderToRight = distanceToRight > contentWidthBiggerThanLabel;
+      const contentPlacementOffset = this.props.contentPlacementOffset;
+
+      const offset = renderToRight
+        ? { left: contentPlacementOffset }
+        : { right: contentPlacementOffset };
+      // set a min-width if the content is narrower than the label
+      const minWidth = contentWidth < labelWidth ? { minWidth: labelWidth } : null;
+
+      return { ...offset, ...minWidth };
+    }
+    return {};
+  }
+
+  render() {
+    const {
+      rootClassName,
+      className,
+      id,
+      name,
+      label,
+      initialValues,
+      contentPlacementOffset,
+      onSubmit,
+      urlParam,
+      intl,
+      showAsPopup,
+      ...rest
+    } = this.props;
+
+    const classes = classNames(rootClassName || css.root, className);
+
+    const hasInitialValues = !!initialValues && initialValues.length > 0;
+    const labelForPopup = hasInitialValues
+      ? intl.formatMessage({ id: 'KeywordFilter.labelSelected' }, { labelText: initialValues })
+      : label;
+
+    const labelForPlain = hasInitialValues
+      ? intl.formatMessage(
+          { id: 'KeywordFilterPlainForm.labelSelected' },
+          { labelText: initialValues }
+        )
+      : label;
+
+    const filterText = intl.formatMessage({ id: 'KeywordFilter.filterText' });
+
+    const placeholder = intl.formatMessage({ id: 'KeywordFilter.placeholder' });
+
+    const contentStyle = this.positionStyleForContent();
+
+    // pass the initial values with the name key so that
+    // they can be passed to the correct field
+    const namedInitialValues = { [name]: initialValues };
+
+    const handleSubmit = (urlParam, values) => {
+      const usedValue = values ? values[name] : values;
+      onSubmit(urlParam, usedValue);
+    };
+
+    const debouncedSubmit = debounce(handleSubmit, DEBOUNCE_WAIT_TIME, {
+      leading: false,
+      trailing: true,
+    });
+    // Use timeout for shart queries and debounce for queries with any length
+    const handleChangeWithDebounce = (urlParam, values) => {
+      // handleSubmit gets urlParam and values as params.
+      // If this field ("keyword") is short, create timeout
+      const hasKeywordValue = values && values[name];
+      const keywordValue = hasKeywordValue ? values && values[name] : '';
+      if (urlParam && (!hasKeywordValue || (hasKeywordValue && keywordValue.length >= 3))) {
+        if (this.shortKeywordTimeout) {
+          window.clearTimeout(this.shortKeywordTimeout);
+        }
+        return debouncedSubmit(urlParam, values);
+      } else {
+        this.shortKeywordTimeout = window.setTimeout(() => {
+          // if mobileInputRef exists, use the most up-to-date value from there
+          return this.mobileInputRef && this.mobileInputRef.current
+            ? handleSubmit(urlParam, { ...values, [name]: this.mobileInputRef.current.value })
+            : handleSubmit(urlParam, values);
+        }, TIMEOUT_FOR_SHORT_QUERIES);
+      }
+    };
+
+    // Uncontrolled input needs to be cleared through the reference to DOM element.
+    const handleClear = () => {
+      if (this.mobileInputRef && this.mobileInputRef.current) {
+        this.mobileInputRef.current.value = '';
+      }
+    };
+
+    return showAsPopup ? (
+      <FilterPopup
+        className={classes}
+        rootClassName={rootClassName}
+        popupClassName={css.popupSize}
+        name={name}
+        label={labelForPopup}
+        isSelected={hasInitialValues}
+        id={`${id}.popup`}
+        showAsPopup
+        labelMaxWidth={250}
+        contentPlacementOffset={contentPlacementOffset}
+        onSubmit={handleSubmit}
+        initialValues={namedInitialValues}
+        urlParam={urlParam}
+        keepDirtyOnReinitialize
+        {...rest}
+      >
+        <FieldTextInput
+          className={css.field}
+          name={name}
+          id={`${id}-input`}
+          type="text"
+          label={filterText}
+          placeholder={placeholder}
+          autoComplete="off"
+        />
+      </FilterPopup>
+    ) : (
+      <FilterPlain
+        className={className}
+        rootClassName={rootClassName}
+        label={labelForPlain}
+        isSelected={hasInitialValues}
+        id={`${id}.plain`}
+        liveEdit
+        contentPlacementOffset={contentStyle}
+        onSubmit={handleChangeWithDebounce}
+        onClear={handleClear}
+        initialValues={namedInitialValues}
+        urlParam={urlParam}
+        {...rest}
+      >
+        <fieldset className={css.fieldPlain}>
+          <label>{filterText}</label>
+          <FieldTextInput
+            name={name}
+            id={`${id}-input`}
+            isUncontrolled
+            inputRef={this.mobileInputRef}
+            type="text"
+            placeholder={placeholder}
+            autoComplete="off"
+          />
+        </fieldset>
+      </FilterPlain>
+    );
+  }
+}
+
+KeywordFilter.defaultProps = {
+  rootClassName: null,
+  className: null,
+  initialValues: null,
+  contentPlacementOffset: 0,
+};
+
+KeywordFilter.propTypes = {
+  rootClassName: string,
+  className: string,
+  id: string.isRequired,
+  name: string.isRequired,
+  urlParam: string.isRequired,
+  label: string.isRequired,
+  onSubmit: func.isRequired,
+  initialValues: string,
+  contentPlacementOffset: number,
+
+  // form injectIntl
+  intl: intlShape.isRequired,
+};
+
+export default injectIntl(KeywordFilter);

src/components/SearchFiltersMobile/SearchFiltersMobile.js
@@ -11,6 +11,7 @@ import { createResourceLocatorString } from '../../util/routes';
 import {
   ModalInMobile,
   Button,
+  KeywordFilter,
   PriceFilter,
   SelectSingleFilter,
   SelectMultipleFilter,
@@ -34,6 +35,7 @@ class SearchFiltersMobileComponent extends Component {
     this.handleSelectMultiple = this.handleSelectMultiple.bind(this);
     this.handlePrice = this.handlePrice.bind(this);
     this.handleDateRange = this.handleDateRange.bind(this);
+    this.handleKeyword = this.handleKeyword.bind(this);
     this.initialValue = this.initialValue.bind(this);
     this.initialValues = this.initialValues.bind(this);
     this.initialPriceRangeValue = this.initialPriceRangeValue.bind(this);
@@ -118,6 +120,15 @@ class SearchFiltersMobileComponent extends Component {
     history.push(createResourceLocatorString('SearchPage', routeConfiguration(), {}, queryParams));
   }
 
+  handleKeyword(urlParam, keywords) {
+    const { urlQueryParams, history } = this.props;
+    const queryParams = urlParam
+      ? { ...urlQueryParams, [urlParam]: keywords }
+      : omit(urlQueryParams, urlParam);
+
+    history.push(createResourceLocatorString('SearchPage', routeConfiguration(), {}, queryParams));
+  }
+
   // Reset all filter query parameters
   resetAll(e) {
     const { urlQueryParams, history, filterParamNames } = this.props;
@@ -185,6 +196,7 @@ class SearchFiltersMobileComponent extends Component {
       amenitiesFilter,
       priceFilter,
       dateRangeFilter,
+      keywordFilter,
       intl,
     } = this.props;
 
@@ -258,12 +270,30 @@ class SearchFiltersMobileComponent extends Component {
     const dateRangeFilterElement =
       dateRangeFilter && dateRangeFilter.config.active ? (
         <BookingDateRangeFilter
-          id="SearchFilters.dateRangeFilter"
+          id="SearchFiltersMobile.dateRangeFilter"
           urlParam={dateRangeFilter.paramName}
           onSubmit={this.handleDateRange}
           liveEdit
           showAsPopup={false}
           initialValues={initialDateRange}
+        />
+      ) : null;
+
+    const initialKeyword = this.initialValue(keywordFilter.paramName);
+    const keywordLabel = intl.formatMessage({
+      id: 'SearchFiltersMobile.keywordLabel',
+    });
+    const keywordFilterElement =
+      keywordFilter && keywordFilter.config.active ? (
+        <KeywordFilter
+          id={'SearchFiltersMobile.keywordFilter'}
+          name="keyword"
+          urlParam={keywordFilter.paramName}
+          label={keywordLabel}
+          onSubmit={this.handleKeyword}
+          liveEdit
+          showAsPopup={false}
+          initialValues={initialKeyword}
         />
       ) : null;
 
@@ -299,6 +329,7 @@ class SearchFiltersMobileComponent extends Component {
           </div>
           {this.state.isFiltersOpenOnMobile ? (
             <div className={css.filtersWrapper}>
+              {keywordFilterElement}
               {categoryFilterElement}
               {amenitiesFilterElement}
               {priceFilterElement}