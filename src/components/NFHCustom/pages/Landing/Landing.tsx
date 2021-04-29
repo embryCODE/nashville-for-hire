77c4ea591ded1fc7654d7586bb016cb2cb22d460
Mason Embry <mason@embrycode.com>
2021-04-29 09:54:11 -0500

    Remove more unneeded parts of home page

--- src/components/NFHCustom/pages/Landing/Landing.tsx
+++ src/components/NFHCustom/pages/Landing/Landing.tsx
@@ -1,9 +1,7 @@
 import React from 'react'
 import styled from 'styled-components/macro'
 import { Constrainer } from '../../layout'
-import guitarOnBed from '../../../../assets/images/guitarOnBed.jpg'
-import { InstrumentTiles, Testimonials, TestimonialVideo } from '../../organisms'
-import ScrollAnimation from 'react-animate-on-scroll'
+import { InstrumentTiles } from '../../organisms'
 
 const WhatDoesYourProjectNeed = styled.h2`
   font-family: Source Sans Pro, sans-serif;
@@ -11,7 +9,7 @@ const WhatDoesYourProjectNeed = styled.h2`
   text-align: right;
   width: 1100px;
   max-width: 100%;
-  margin: 2rem auto;
+  margin: 1rem auto 2rem;
   padding: 0 2rem;
 
   @media (max-width: 600px) {
@@ -20,66 +18,15 @@ const WhatDoesYourProjectNeed = styled.h2`
   }
 `
 
-const TestimonialsHero = styled.div`
-  display: flex;
-  align-items: center;
-  margin-bottom: 3rem;
-
-  h2 {
-    margin-left: 6rem;
-    font-family: Source Sans Pro, sans-serif;
-    font-size: 36px;
-  }
-
-  @media (max-width: 600px) {
-    flex-direction: column;
-
-    div {
-      display: none;
-    }
-
-    h2 {
-      margin-left: 0;
-    }
-  }
-`
-
-const BackgroundColorDiv = styled.div`
-  background-color: #ac7163;
-  width: auto;
-  max-width: 400px;
-  padding: 2rem 0;
-
-  img {
-    position: relative;
-    right: -3rem;
-    max-width: 400px;
-  }
-`
-
 export const Landing: React.FC = () => {
   return (
     <div css={{ backgroundColor: '#f9f4ee' }}>
       <Constrainer>
+        <h1 style={{ marginTop: '2rem' }}>Nashville For Hire</h1>
+
         <WhatDoesYourProjectNeed>What does your project need?</WhatDoesYourProjectNeed>
 
         <InstrumentTiles />
-
-        <TestimonialsHero>
-          <ScrollAnimation animateIn="fadeInLeft">
-            <BackgroundColorDiv>
-              <img alt="A person playing guitar" src={guitarOnBed} />
-            </BackgroundColorDiv>
-          </ScrollAnimation>
-
-          <ScrollAnimation animateIn="fadeInDown">
-            <h2>What are our clients saying about us?</h2>
-          </ScrollAnimation>
-        </TestimonialsHero>
-
-        <Testimonials />
-
-        <TestimonialVideo />
       </Constrainer>
     </div>
   )
